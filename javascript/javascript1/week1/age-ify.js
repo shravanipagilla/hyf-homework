ce9c5e5baabe7f80db14250979e72f00ce91c313
shravanipagilla <nampally04@gmail.com>
2021-06-30 00:42:03 +0200

    added files

---
+++ javascript/javascript1/week1/age-ify.js
@@ -0,0 +1,6 @@
+const yearOfBirth = 1992;
+let yearFuture = 2045;
+console.log("typeof the variable 'yearOfBirth' is: " + typeof(yearOfBirth));
+console.log("typeof the variable 'yearFuture' is: " + typeof(yearFuture));
+let age = yearFuture - yearOfBirth;
+console.log("you will be " + age + " years old in " + yearFuture);
