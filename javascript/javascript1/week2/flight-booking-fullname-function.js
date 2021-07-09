af47e78b738f9bc9064f0c9c218e0e4c0652e745
shravanipagilla <nampally04@gmail.com>
2021-07-09 10:29:41 +0200

    flight-booking-fullname-function.js file added

---
+++ javascript/javascript1/week2/flight-booking-fullname-function.js
@@ -0,0 +1,31 @@
+// Function getFullaname() returns full name with/without Formal
+
+
+function getFullname(firstname, surname, useFormalName) {
+    if (!firstname) return new Error("Firstname  must not be null");
+    else if (!surname) return new Error("Surname must not be null");
+    else if (!useFormalName) return "Lord" + " " + firstname + " " + surname;
+    else return firstname + " " + surname;
+}
+
+var fullname1 = getFullname("Benjamin", "Hughes", true); // returns "Lord Benjamin Hughes"
+var fullname2 = getFullname("Benjamin", "Hughes", false); // returns "Benjamin Hughes"
+
+console.log(fullname1);
+console.log(fullname2);
+
+/* Answer to the Question 
+'I add one more argument named 'isMale' to the function, based on this boolian value
+ suffix Lord or Lady to fullName'
+
+function getFullname(firstname, surname, useFormalName, isMale) {
+firstname
+    if (!firstname) return new Error("Firstname  must not be null");
+    if (!surname) return new Error("Surname must not be null");
+    if (!useFormalName) return firstname + " " + surname;
+    if (useFormalName && isMale) return "Lord" + " " + firstname + " " + surname;
+    if (useFormalName && !isMale) return "Lady" + " " + firstname + " " + surname;
+
+}
+var fullname1 = getFullname("Benjamin", "Hughes", true, false); // returns "Lady Benjamin Hughes"
+*/
