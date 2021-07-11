dbab122b7f74da8a8b941e0a91d945ca54ef8622
shravanipagilla <nampally04@gmail.com>
2021-07-11 22:19:55 +0200

    weather-wear.js added

--- javascript/javascript1/week2/weather-wear.js
+++ javascript/javascript1/week2/weather-wear.js
@@ -1,10 +1,12 @@
 function clothesRecomended(temperature) {
-    if (temperature > 17)
+    if (temperature > 17) {
         console.log("shorts and a t-shirt");
-    else if (temperature > 10 && temperature <= 17)
+    } else if (temperature > 10 && temperature <= 17) {
         console.log("Hoodie-Jeans");
-    else if (temperature <= 10)
+    } else if (temperature <= 10) {
         console.log("Winter Jacket and winter pants with shoes");
-    else console.log("Un-known temperature");
+    } else {
+        console.log("Un-known temperature");
+    }
 }
 clothesRecomended(18); // Logs out: "shorts and a t-shirt"
