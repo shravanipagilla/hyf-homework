dc939a240c65b0c7be39cae2ff6e073a697ac131
shravanipagilla <nampally04@gmail.com>
2021-07-11 22:20:07 +0200

    candy-helper.js added

--- javascript/javascript1/week2/candy-helper.js
+++ javascript/javascript1/week2/candy-helper.js
@@ -4,46 +4,40 @@ var amountToSpend = Math.random() * 100;
 function addCandy(candyType, weight) {
     switch (candyType) {
         case 'Sweet':
-            return boughtCandyPrices.push(candyType, weight * 0.5);
+            boughtCandyPrices.push(candyType, weight * 0.5);
             break;
         case 'Chocolate':
-            return boughtCandyPrices.push(candyType, weight * 0.7);
+            boughtCandyPrices.push(candyType, weight * 0.7);
             break;
         case 'Toffee':
-            return boughtCandyPrices.push(candyType, weight * 1.1);
+            boughtCandyPrices.push(candyType, weight * 1.1);
             break;
         case 'Chewing-gum':
-            return boughtCandyPrices.push(candyType, weight * 0.03);
+            boughtCandyPrices.push(candyType, weight * 0.03);
             break;
         default:
-            return console.log("Item not available");
+            console.log("Item not available");
             break;
     }
 }
 
 function canBuyMoreCandy() {
-    let canBuyMore;
-    let prices = boughtCandyPrices.filter((element, index) => {
-            return index % 2 !== 0;
-        }) //All odd indexed values stored into prices a array
-    var total = 0;
-    for (var i in prices) {
-        total += prices[i];
-    } // for loop can be replaced by: prices.reduce((x, y) => x + y);
-    if (total < amountToSpend)
-        canBuyMoreCandy = true;
-    else
-        canBuyMoreCandy = false;
-    return canBuyMoreCandy;
+
+    let total = 0;
+    for (let i = 0; i < boughtCandyPrices.length; i++) {
+        if (i % 2 !== 0) {
+            total += boughtCandyPrices[i];
+        }
+    }
+    if (total < amountToSpend) { return true; } else { return false; }
+
 }
+
 addCandy('Sweet', 21);
 addCandy('Toffee', 30);
 addCandy('Chocolate', 50);
 addCandy('Chewing-gum', 45);
 console.log(boughtCandyPrices);
-
+canBuyMoreCandy();
 let result = canBuyMoreCandy();
-if (result == true)
-    console.log("You can buy more, so please do!");
-else
-    console.log("Enough candy for you!");
+if (result === true) { console.log("You can buy more, so please do!"); } else { console.log("Enough candy for you!"); }
