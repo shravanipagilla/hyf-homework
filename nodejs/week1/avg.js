1d4cd84338b151980287d075d428a2bfe15a046e
shravanipagilla <nampally04@gmail.com>
2022-09-12 23:27:48 +0200

    made changes ...

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -4,5 +4,10 @@ const myArgs = process.argv.slice(2)
 const sum  = myArgs.reduce((a,b) => {
     return Number(a)+ Number(b)
  }, 0);
-
+if(isNaN(sum)){
+  console.log('Please enter only numbers');
+}
+else{
  console.log('Average of given numbers  :' + sum / myArgs.length );
+}
+
