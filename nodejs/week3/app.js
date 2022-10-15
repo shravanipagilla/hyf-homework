b42e4b9562a6e80fb720ce75b073f0e35374ce64
shravanipagilla <nampally04@gmail.com>
2022-10-15 15:09:43 +0200

    comments resolved

--- nodejs/week3/app.js
+++ nodejs/week3/app.js
@@ -1,48 +1,52 @@
 // @ts-nocheck
 const knex = require("knex")({
-    client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST || "127.0.0.1",
-      port: process.env.DB_PORT || 33061,
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "mypassword",
-      database: process.env.DB_NAME || "hyf_node_week3_warmup",
-      multipleStatements: true,
-    },
-  })
-  
-  const express = require("express")
-  const app = express()
-  const port = process.env.PORT || 3000
-  
-  app.use(express.json())
-  
-  const apiRouter = express.Router()
-  app.use("/api", apiRouter)
-  
-  const contactsAPIRouter = express.Router()
-  apiRouter.use("/contacts", contactsAPIRouter)
-  
-  contactsAPIRouter.get("/", async (req, res) => {
-    let query = knex.select("*").from("contacts")
-    if ("sort" in req.query) {
-      const orderBy = req.query.sort.toString()
-      if (orderBy.length > 0) {
-        query = query.orderBy(orderBy)
-      }
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 33061,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "bharath143",
+    database: process.env.DB_NAME || "hyf_node_week3_warmup",
+    multipleStatements: true,
+  },
+});
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const apiRouter = express.Router();
+app.use("/api", apiRouter);
+
+const contactsAPIRouter = express.Router();
+apiRouter.use("/contacts", contactsAPIRouter);
+
+contactsAPIRouter.get("/", async (req, res) => {
+  /*.select()
+  .table('products')
+  .orderBy('name', 'desc') */
+  let query = knex.select("*").table("contacts");
+  if ("sort" in req.query) {
+    const orderBy = req.query.sort.toString();
+    const paramFields=orderBy.trim(). split(/\s+/) ;
+    if (orderBy.length > 0) {
+      query = query.orderBy(paramFields[0],paramFields[1]);
     }
-  
-    console.log("SQL", query.toSQL().sql)
-  
-    try {
-      const data = await query
-      res.json({ data })
-    } catch (e) {
-      console.error(e)
-      res.status(500).json({ error: "Internal server error" })
-    }
-  })
-  
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-  })
+  }
+
+  console.log("SQL", query.toSQL().sql);
+
+  try {
+    const data = await query;
+    res.json({ data });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
